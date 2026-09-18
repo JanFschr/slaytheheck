93a87cfc714a7f77563ef060e32faff87ee02709
JanFschr <JanFschr@users.noreply.github.com>
2026-09-18 11:40:11 +0200

    Polish cyberpunk start room layout

--- src/ui/components/start-room.js
+++ src/ui/components/start-room.js
@@ -3,11 +3,11 @@ import {Component, html} from '../lib.js'
 export default class StartRoom extends Component {
 	render() {
 		return html`
-			<div class="Container Container--center">
-				<div class="Box">
-				<h1 center style="color: black">It begins…</h1>
-				<p center>Fight your way through the dungeon, build your deck and defeat the boss at the end.</p>
-					<ul class="Options">
+			<div class="Container Container--center StartRoom">
+				<div class="Box StartRoom-panel">
+					<h1 class="StartRoom-title">It begins…</h1>
+					<p class="StartRoom-copy">Fight your way through the dungeon, build your deck and defeat the boss at the end.</p>
+					<ul class="Options StartRoom-actions">
 						<li><button class="Button" onClick=${() => this.props.onContinue()}>Open the map</button></li>
 					</ul>
 				</div>
